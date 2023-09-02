43d0f767f7d8bb2a6abdf2c621986378c39a283e
hengxin6315 <heng.xin@hellogroup.com>
2023-09-02 16:58:41 +0800

    app底部导航类似咸鱼

---
+++ android/app/src/main/kotlin/com/example/flutterdemo07/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.flutterdemo07
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/pages/tabs.dart
@@ -0,0 +1,71 @@
+import 'package:flutter/material.dart';
+import './tabs/home.dart';
+import './tabs/category.dart';
+import './tabs/message.dart';
+import './tabs/setting.dart';
+import './tabs/user.dart';
+
+class Tabs extends StatefulWidget {
+  const Tabs({super.key});
+
+  @override
+  State<Tabs> createState() => _TabsState();
+}
+
+class _TabsState extends State<Tabs> {
+  int _currentIndex = 0;
+  final List<Widget> _pages = const [
+    HomePage(),
+    CategoryPage(),
+    MessagePage(),
+    SettingPage(),
+    UserPage()
+  ];
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text("Flutter App")),
+      body: _pages[_currentIndex],
+      bottomNavigationBar: BottomNavigationBar(
+          fixedColor: Colors.red, //选中的颜色
+          // iconSize:35,           //底部菜单大小
+          currentIndex: _currentIndex, //第几个菜单选中
+          type: BottomNavigationBarType.fixed, //如果底部有4个或者4个以上的菜单的时候就需要配置这个参数
+          onTap: (index) {
+            //点击菜单触发的方法
+            //注意
+            setState(() {
+              _currentIndex = index;
+            });
+          },
+          items: const [
+            BottomNavigationBarItem(icon: Icon(Icons.home), label: "首页"),
+            BottomNavigationBarItem(icon: Icon(Icons.category), label: "分类"),
+            BottomNavigationBarItem(icon: Icon(Icons.message), label: "消息"),
+            BottomNavigationBarItem(icon: Icon(Icons.settings), label: "设置"),
+            BottomNavigationBarItem(icon: Icon(Icons.people), label: "用户")
+          ]),
+      floatingActionButton: Container(
+        height: 60,  //调整FloatingActionButton的大小
+        width: 60,
+        padding: const EdgeInsets.all(5),
+        margin: const EdgeInsets.only(top: 5),  //调整FloatingActionButton的位置
+        decoration: BoxDecoration(
+          color: Colors.white,
+          borderRadius: BorderRadius.circular(30),
+        ),
+        child: FloatingActionButton(
+            backgroundColor:_currentIndex == 2 ? Colors.red : Colors.blue,
+            child: const Icon(Icons.add),
+            onPressed: () {
+              setState(() {
+                _currentIndex=2;
+              });
+            }
+        ),
+      ),
+      floatingActionButtonLocation:
+      FloatingActionButtonLocation.centerDocked, //配置浮动按钮的位置
+    );
+  }
+}
